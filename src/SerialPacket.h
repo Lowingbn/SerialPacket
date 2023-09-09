ee37c0b69d97b78f79ff9dd965e53c569014f4d1
Mike Bell <mike@mercuna.com>
2023-09-09 18:19:07 +0100

    Ability to mix packets and string commands

--- src/SerialPacket.h
+++ src/SerialPacket.h
@@ -82,9 +82,12 @@ private:
 
 // Packet class for recieving data from serial connection
 // The same SerialInPacket can be used repeatedly.
-// Once Available return trues the entire packet data must be read before the
-// next call to Available, as at that point the previous packet will be invalidated
-template<const uint8_t MAX_DATA_SIZE = 16>
+// Strings and packets can be received.  A packet has type-length-data format and
+// starts with a type <= maxCommandChar.  A string starts with any other character
+// and continues until a newline is sent.
+// This allows the same serial interface to be used for both string console commands
+// and binary packets.
+template<const uint32_t MAX_DATA_SIZE = 1024>
 class SerialInPacket
 {
 public:	
@@ -92,6 +95,13 @@ public:
     {
         m_pPort = &port;
     }
+
+    enum class EAvailableStatus : uint8_t
+    {
+        None = 0,
+        StringAvailable,  // A string terminated with \n is available
+        PacketAvailable,  // A serial packet is available
+    };
 
     bool Available()
     {
@@ -102,7 +112,14 @@ public:
             if (m_state == EReadState::ReadType)
             {
                 m_type = recChar;
-                m_state = EReadState::ReadDataLen;
+                if (m_type > maxCommandChar)
+                {
+                    m_state = EReadState::ReadString;
+                }
+                else
+                {
+                    m_state = EReadState::ReadDataLen;
+                }
                 continue;
             }
             else if (m_state == EReadState::ReadDataLen)
@@ -142,7 +159,39 @@ public:
                     m_state = EReadState::ReadType;
                     m_dataLen = (m_dataLen > MAX_DATA_SIZE ? MAX_DATA_SIZE : m_dataLen);
                     m_dataPtr = m_data;
-                    return true;
+                    return EAvailableStatus::PacketAvailable;
+                }
+            }
+            else if (m_state == EReadState::ReadString)
+            {
+                if (m_curDataLen < MAX_DATA_SIZE)
+                {
+                    // Add the incoming data to the buffer
+                    m_data[m_curDataLen++] = recChar;
+                }
+                else
+                {
+                    // Not room left to store the incoming data in the buffer so
+                    // just discard instead
+                    m_curDataLen++;
+                }
+
+                if (recChar == endStringChar)
+                {
+                    // Terminate string
+                    if (m_curDataLen < MAX_DATA_SIZE)
+                    {
+                        m_data[m_curDataLen] = 0;
+                    }
+                    else
+                    {
+                        m_data[MAX_DATA_SIZE-1] = 0;
+                    }
+
+                    m_state = EReadState::ReadType;
+                    m_dataLen = (m_curDataLen > MAX_DATA_SIZE ? MAX_DATA_SIZE : m_curDataLen);
+                    m_dataPtr = m_data;
+                    return EAvailableStatus::StringAvailable;
                 }
             }
         }
@@ -153,6 +202,16 @@ public:
     const byte GetType() const
     {
         return m_type;
+    }
+
+    const char* GetStr() const
+    {
+        return (const char*)m_data;
+    }
+
+    const int GetStrLen() const
+    {
+        return m_dataLen;
     }
 
     template<typename T>
@@ -180,14 +239,18 @@ private:
         ReadType,
         ReadDataLen,
         ReadData,
+        ReadString,
     };
 
     Stream* m_pPort = nullptr;
 
+    static const byte endStringChar = '\n';
+    static const byte maxCommandChar = ' ';
+
     EReadState m_state = EReadState::ReadType;
     byte m_type;
-    byte m_dataLen;
-    byte m_curDataLen;
+    uint32_t m_dataLen;
+    uint32_t m_curDataLen;
     byte m_data[MAX_DATA_SIZE];
     byte* m_dataPtr;
 };
